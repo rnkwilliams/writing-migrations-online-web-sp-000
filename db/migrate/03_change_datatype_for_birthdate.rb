71241f8464694316092478faa80bbc4968765e24
Ranika Williams <rnkprice07@gmail.com>
2019-08-18 16:18:53 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,7 +1,7 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
 
   def change
-    add_column :students, :birthdate, :datetime
+    change_column :students, :birthdate, :datetime
   end
 
 end
